952befafccb0ccba9ea52c40889318060bf57092
Lawrence Chong <lawrencechong@Lawrences-Air.home>
2016-02-09 20:59:59 -0500

    Refined movie detail view

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet weak var posterImageView: UIImageView!
     
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var overviewLabel: UILabel!
     
+    @IBOutlet weak var infoView: UIView!
+    
+    
     var movie : NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
         
         let title = movie["title"] as? String
         titleLabel.text = title
         
         let overview = movie["overview"] as? String
         overviewLabel.text = overview
-        
+        overviewLabel.sizeToFit()
+    
         if let posterPath = movie["poster_path"] as? String{
             let base_url = "http://image.tmdb.org/t/p/w500"
             let image_url = NSURL(string: base_url + posterPath)
